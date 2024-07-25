77f980ff279453972473a8a3d7d247f8bab66873
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-07-25 20:24:12 -0300

    feat: fetch activities

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,37 +1,63 @@
 import { CircleCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
+import { format, parseISO } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+interface Activity {
+    id: string;
+    title: string;
+    occursAt: string;
+}
 
 export function Activities() {
-    return(
+    const { tripId } = useParams();
+    const [activities, setActivities] = useState<Activity[] | undefined>(undefined);
+
+    useEffect(() => {
+        const fetchActivities = async () => {
+            try {
+                const response = await api.get(`/trips/${tripId}/activities`);
+                
+                setActivities(response.data);
+            } catch (error) {
+                console.error("Failed to fetch activities:", error);
+            }
+        };
+       
+        fetchActivities();
+    }, [tripId]);
+   
+    return (
         <div className="space-y-8">
-            <div className="space-y-2.5">
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-                    <span className="text-xs text-zinc-500">Sábado</span>
-                </div>
-                <p className="text-zinc-500 text-sm">Nunhuma atividade cadastrada nessa data.</p>
-            </div>
+            {activities?.map(activity => {                
+                const activityTime = parseISO(activity.occursAt);
+                const activityDate = format(activityTime, 'dd');
+                const activityDay = format(activityTime, 'EEEE, dd MMMM', { locale: ptBR });
 
-            <div className="space-y-2.5">
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-                    <span className="text-xs text-zinc-500">Domingo</span>
-                </div>
-                <div className="space-y-2.5">
-                    <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                        <CircleCheck className="size-5 text-lime-300"/>
-                        <span className="text-zinc-100">Acedemia em grupo</span>
-                        <span className="text-zinc-400 text-sm ml-auto">18:00h</span>
+                return (
+                    <div key={activity.id} className="space-y-2.5">
+                        <div className="flex gap-2 items-baseline">
+                            <span className="text-xl text-zinc-300 font-semibold">
+                                Dia {activityDate}
+                            </span>
+                            <span className="text-xs text-zinc-500">
+                                {activityDay}
+                            </span>
+                        </div>
+                        <div className="space-y-2.5">
+                            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                                <CircleCheck className="size-5 text-lime-300" />
+                                <span className="text-zinc-100">{activity.title}</span>
+                                <span className="text-zinc-400 text-sm ml-auto">
+                                    {format(activityTime, 'HH:mm')}h
+                                </span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div className="space-y-2.5">
-                    <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
-                        <CircleCheck className="size-5 text-lime-300"/>
-                        <span className="text-zinc-100">Acedemia em grupo</span>
-                        <span className="text-zinc-400 text-sm ml-auto">18:00h</span>
-                    </div>
-                </div>
-            </div>                        
+                );
+            })}                                    
         </div>
-    )
+    );
 }
--- src/pages/trip-details/destination-and-date-header.tsx
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -1,18 +1,52 @@
 import { MapPin, Calendar, Settings2 } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { api } from "../../lib/axios";
+import { format, parseISO } from "date-fns";
+
+interface Trip {
+    id: string;
+    destination: string;
+    startsAt: string;
+    endsAt: string;
+    is_confirmed: boolean;
+}
 
 export function DestinationAndDateHeader() {
-    return(
+    const [trip, setTrip] = useState<Trip | undefined>(undefined);
+    const { tripId } = useParams();
+
+    useEffect(() => {
+        const fetchTrip = async () => {
+            try {
+                const response = await api.get(`/trips/${tripId}`);
+               
+                setTrip(response.data);
+                
+            } catch (error) {
+                console.error("Failed to fetch trip:", error);
+            }
+        };
+
+        fetchTrip();
+    }, [tripId]);      
+    
+    const displayedDate = trip
+        ? format(parseISO(trip.startsAt), "dd' de 'LLL").concat(' até ').concat(format(parseISO(trip.endsAt), "dd' de 'LLL"))
+        : null;
+
+    return (
         <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
             <div className="flex items-center gap-2">
-                <MapPin className="size-5 text-zinc-400"/>
-                <span className="text-zinc-100">Florianópolis, Brasil</span>
+                <MapPin className="size-5 text-zinc-400" />
+                <span className="text-zinc-100">{trip?.destination}</span>
             </div>
 
             <div className="flex items-center gap-5">
                 <div className="flex items-center gap-2">
-                    <Calendar className="size-5 text-zinc-400"/>
-                    <span className="text-zinc-100">17 a 23 de Agosto</span>
+                    <Calendar className="size-5 text-zinc-400" />
+                    <span className="text-zinc-100">{displayedDate}</span>
                 </div>
 
                 <div className="w-px h-6 bg-zinc-800"></div>
@@ -23,5 +57,5 @@ export function DestinationAndDateHeader() {
                 </Button>
             </div>
         </div>
-    )
+    );
 }
