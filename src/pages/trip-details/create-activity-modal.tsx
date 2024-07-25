687cf9aeea7fe7c0830cdd2e838702607c92db17
TiagoCoder2022 <tiagonewalves@hotmail.com>
2024-07-25 20:27:01 -0300

    feat: post activites

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,11 +1,32 @@
 import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateActivityModalProps {
     closeCreateActiveModal: () => void
 }
 
 export function CreateActivityModal({closeCreateActiveModal}: CreateActivityModalProps) {
+    const { tripId } = useParams()
+
+    async function createActivity(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        const data = new FormData(e.currentTarget)
+
+        const title = data.get('title')?.toString()
+        const occurs_at = data.get('occurs_at')?.toString()
+        
+        await api.post(`/trips/${tripId}/activities`, {
+            title,
+            occurs_at
+        })
+
+       window.document.location.reload()
+    }
+
     return (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
             <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -17,11 +38,11 @@ export function CreateActivityModal({closeCreateActiveModal}: CreateActivityModa
                         </button>
                     </div>
                     <p className="text-zinc-400 text-sm">
-                            Todos os convidados podem vizualizar as atividades.
+                        Todos os convidados podem vizualizar as atividades.
                     </p>
                 </div>                   
         
-                <form className="space-y-3">
+                <form onSubmit={createActivity} className="space-y-3">
                     <div className="h-12 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                         <Tag className="text-zinc-400"/>
                         <input 
